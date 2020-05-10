22bac86d50dc0d2f4e3d1059e6a4460ae1e1ff0e
GoodRyanUS <usafaryangood@gmail.com>
2020-05-09 23:13:35 -0400

    Add basic controller init

---
+++ c2/agent.py
@@ -0,0 +1,4 @@
+class Agent:
+    def __init__(self, address, port):
+        self.address = address
+        self.port = port
--- c2/c2.py
+++ c2/c2.py
@@ -1,8 +1,32 @@
 #!/bin/python3.7
+import socket
 from pathlib import Path
 
 import sys
 sys.path.append(str(Path(__file__).resolve().parents[1]))
 
-import protocol.command_pb2
+from protocol.command_pb2 import Command
+from agent import Agent
+
+
+class Controller:
+    def __init__(self):
+        server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server.setblocking(0)
+        server_address = ('0', 9999)
+        server.bind(server_address)
+        server.listen(5)
+
+    def send_command(self, agent, command):
+        pass 
+
+
+'''For testing'''
+command = None
+with open('placeholder', 'rb') as f:
+    command = Command.FromString(f.read())
+'''For testing'''
+
+controller = Controller()
+agent = Agent('127.1', '1337')
 
