438f60cda13094a6b5d891d0e98d564a3042a0a8
GoodRyanUS <usafaryangood@gmail.com>
2020-05-12 12:44:48 -0400

    Add more fleshed out c2 structure

--- c2/agent.py
+++ c2/agent.py
@@ -1,4 +1,6 @@
+from collections import deque
 class Agent:
-    def __init__(self, address, port):
-        self.address = address
-        self.port = port
+    def __init__(self, conn, id_no):
+        self.id = id_no
+        self.conn = conn 
+        self.data_q = deque()
--- c2/c2.py
+++ c2/c2.py
@@ -8,10 +8,11 @@ sys.path.append(str(Path(__file__).resolve().parents[1]))
 
 from protocol.command_pb2 import Command
 from agent import Agent
+from operator import Operator
 
 
 class Controller:
-    def __init__(self, interface='0', port=9999):
+    def __init__(self, interface='0', port=9999, backlog=5):
         self.selector = selectors.DefaultSelector()
 
         self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -19,8 +20,11 @@ class Controller:
         self.server.setblocking(False)
         self.server_address = (interface, port)
         self.server.bind(self.server_address)
-        self.server.listen(5)
-        self.selector.register(self.server, selectors.EVENT_READ, self.accept)
+        self.server.listen(backlog)
+        self.selector.register(self.server, selectors.EVENT_READ, self.accept_ops)
+
+        self.agents = {}
+        self.operators = {}
 
     
     def type_determination(self, conn, mask):
@@ -31,27 +35,37 @@ class Controller:
             return
 
         if identity == "agent":
-            print('agent')
             self.selector.modify(conn, selectors.EVENT_READ | selectors.EVENT_WRITE, self.data_agent)
+            next_agent_id = len(self.agents)
+            agent = Agent(conn, next_agent_id)
+            self.agents[next_agent_id] = agent
             
-        elif identity == "operator":
-            print('ops')
-            self.selector.modify(conn, selectors.EVENT_READ | selectors.EVENT_WRITE, self.data_ops)
-
         else:
             print("invalid:", identity)
             self.selector.unregister(conn)
             conn.close()
 
+    
+    def controller_comm(self, conn, data):
+        data = data.split(" ")
+        if data[0] = "listen":
+            agent_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            agent_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+            agent_sock.setblocking(False)
+            agent_address = (data[1], data[2])
+            agent_sock.bind(agent_address)
+            agent_sock.listen(1)
+            self.selector.register(agent_sock, selectors.EVENT_READ, self.accept_agent)
+
 
     def data_agent(self, conn, mask):
         try:
-            data = conn.recv(1024)
+            data = conn.recv(1024).decode().rstrip()
         except BlockingIOError:
             return
 
         if data:
-            print("agent received", data,"from", conn)
+            print("agent received", data, "from", conn)
         else:
             print("closing", conn)
             self.selector.unregister(conn)
@@ -60,23 +74,31 @@ class Controller:
 
     def data_ops(self, conn, mask):
         try:
-            data = conn.recv(1024)
+            data = conn.recv(1024).decode().rstrip()
         except BlockingIOError:
             return
 
         if data:
             print("ops received", data,"from", conn)
+            operator = self.operators[hash(conn)]
+            if not operator.agent:
+                self.controller_comm(conn, data)
+            else:
+                self.ops_to_agent(conn, data)
         else:
             print("closing", conn)
             self.selector.unregister(conn)
             conn.close()
 
 
-    def accept(self, sock, mask):
+    def accept_ops(self, sock, mask):
         conn, addr = sock.accept()
         print('accepted', conn, 'from', addr)        
+
+        operator = Operator(conn)
+        self.operators[hash(conn)] = operator 
         conn.setblocking(False)
-        self.selector.register(conn, selectors.EVENT_READ | selectors.EVENT_WRITE, self.type_determination)
+        self.selector.register(conn, selectors.EVENT_READ | selectors.EVENT_WRITE, self.data_ops)
 
 
     def handle_agents(self):
@@ -87,8 +109,12 @@ class Controller:
                 callback(key.fileobj, mask)
 
 
-    def send_command(self, agent, command):
+    def ops_to_agent(self, agent, command):
         pass 
+
+
+    def receive_data(self, agent):
+        pass
 
 
 '''For testing'''
@@ -99,5 +125,4 @@ with open('placeholder', 'rb') as f:
 
 controller = Controller()
 controller.handle_agents()
-agent = Agent('127.1', '1337')
 
