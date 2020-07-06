7b098593be775947722ba6711014f42f0171232f
GreaterGoodest <usafaryangood@gmail.com>
2020-07-06 18:58:47 -0500

    Properly collects connection info from user

--- operator/ops_manager.py
+++ operator/ops_manager.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import argparse
 import cmd2
+import socket
 import sys
 
 class OpsManager(cmd2.Cmd):
@@ -8,10 +9,12 @@ class OpsManager(cmd2.Cmd):
     connection_args = argparse.ArgumentParser(description="Connect to C2 Server")
     connection_args.add_argument('host', help='hostname/ip of C2 server to connect to')
     connection_args.add_argument('port', help="port C2 is listening on")
-    
+
     @cmd2.with_argparser(connection_args)
     def do_connect(self, args):
-        pass
+        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server_address = args.host
+        server_port = args.port
 
 
 if __name__ == "__main__":
